f27ba14584eca049b647f62d32b0d19ca0ef8bd4
Mirko <ruhlmirkojoel@gmail.com>
2025-02-05 19:59:22 -0300

    Finished first kata for codewars

--- 01-digital-root.py
+++ 01-digital-root.py
@@ -1,11 +1,29 @@
+# def digital_root(n):
+#     digit_len = len(str(n))
+#     digit_str = str(n)
+#     rootResult = 0
+#     while digit_len > 1:
+#         for i, item in enumerate(digit_str):
+#             rootResult += int(digit_str[i])
+#         digit_str = str(rootResult)
+#         digit_len = len(digit_str)
+#         rootResult = 0
 
-
-n = input('Enter a number: ')
-while n!=0:
+def digital_root(n):
+    digit_len = len(str(n))
+    digit_str = str(n)
     rootResult = 0
+    while digit_len > 1:
+        for i, item in enumerate(digit_str):
+            rootResult += int(digit_str[i])
+        digit_str = str(rootResult)
+        digit_len = len(digit_str)
+        rootResult = 0
+    return int(digit_str)
 
-    for i, item in enumerate(n):
-        print(n[i])
 
-    print(f'The digital root is {rootResult} ')
-    n = input('Enter a new number: ')
+
+print(digital_root(16))
+print(digital_root(942))
+print(digital_root(132189))
+digital_root(493193)
