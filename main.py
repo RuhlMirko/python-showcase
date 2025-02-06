5adda65deab20349b99df72396d1b548e605e15a
Mirko <ruhlmirkojoel@gmail.com>
2025-02-06 14:18:57 -0300

    Added first version of working gui

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import ttkbootstrap as tb
 import lorem
+from showcase import *
 
 OUTPUTS_IMG = 'D:/Descargas/Horarios'
 
@@ -8,7 +9,7 @@ root = tb.Window(themename='darkly')
 
 # Center app on window
 app_width = 500
-app_height = 290
+app_height = 450
 screen_width = root.winfo_screenwidth()
 screen_height = root.winfo_screenheight()
 x = int((screen_width/2) -(app_width/2))
@@ -24,20 +25,24 @@ Style.configure('em.TLabel', font=('Segoe UI', 10, 'italic'), foreground='#666')
 Style.configure('TButton', font=('Segoe UI', 10))
 
 # Title
-title_lbl = tb.Label(text='Title of project', style='titleLabel.TLabel')
+title_lbl = tb.Label(text=return_str()[0], style='titleLabel.TLabel')
 title_lbl.pack(pady=10)
 summary_text = lorem.sentence()
-summary_lbl = tb.Label(text=f'Thus {summary_text}', style='em.TLabel')
+summary_lbl = tb.Label(text=return_str()[1], style='em.TLabel')
 summary_lbl.pack()
-summary_btn = tb.Button(root, text='Read more')
-summary_btn.pack(pady=(0, 20))
 
 # showcase
-example_frame = tb.LabelFrame(root, text='Test')
+example_frame = tb.LabelFrame(root, text='Test Data')
 example_frame.pack(pady=10)
-title_lbl = tb.Label(example_frame, text='PDF Converter', style='titleLabel.TLabel')
-title_lbl.pack(pady=30)
+proj_lbl = tb.Label(example_frame, text=return_str()[2])
+proj_lbl.pack(padx=20, pady=10)
 
-
+# Project cursor
+nav_proj_frame = tb.Frame(root)
+nav_proj_frame.pack(pady=10)
+prevProj = tb.Button(nav_proj_frame, text='Prev', width=10, bootstyle='outline-info')
+nextProj = tb.Button(nav_proj_frame, text='Next', width=10, bootstyle='outline-info')
+prevProj.pack(side='left', padx=10)
+nextProj.pack(side='right', padx=10)
 
 root.mainloop()
--- showcase.py
+++ showcase.py
@@ -1,3 +1,9 @@
 from digitalRoot import *
 import ttkbootstrap as tb
 
+
+list_projects = ['digitalRoot', 'duplicates', 'squareCheck']
+
+
+def return_str(proj_name='Default'):
+    return printData()
