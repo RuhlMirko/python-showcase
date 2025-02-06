ed01438b96e15ba3b17677f7dc501fd5f77050a0
Mirko <ruhlmirkojoel@gmail.com>
2025-02-06 01:12:50 -0300

    Added short version

--- 01-digital-root.py
+++ 01-digital-root.py
@@ -1,29 +1,10 @@
-# def digital_root(n):
-#     digit_len = len(str(n))
-#     digit_str = str(n)
-#     rootResult = 0
-#     while digit_len > 1:
-#         for i, item in enumerate(digit_str):
-#             rootResult += int(digit_str[i])
-#         digit_str = str(rootResult)
-#         digit_len = len(digit_str)
-#         rootResult = 0
-
 def digital_root(n):
-    digit_len = len(str(n))
-    digit_str = str(n)
-    rootResult = 0
-    while digit_len > 1:
-        for i, item in enumerate(digit_str):
-            rootResult += int(digit_str[i])
-        digit_str = str(rootResult)
-        digit_len = len(digit_str)
-        rootResult = 0
-    return int(digit_str)
+    while n >= 10:
+        print(int(digit) for digit in str(n))
+        n = sum(int(digit) for digit in str(n))
+    return n
 
+test_data = [16, 942, 132189, 493193]
+for i in test_data:
+    print(f'The digital root of {i} is: {digital_root(i)}')
 
-
-print(digital_root(16))
-print(digital_root(942))
-print(digital_root(132189))
-digital_root(493193)
